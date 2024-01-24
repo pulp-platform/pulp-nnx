b02e01879df518821c7890a00e45be06a43a794f
Luka Macan <luka.macan@unibo.it>
2024-01-24 18:15:10 +0100

    Align names with Neureka.py

--- test/Ne16.py
+++ test/Ne16.py
@@ -32,63 +32,70 @@ class Ne16:
     ) -> npt.NDArray[np.uint8]:
         """Unroll weight into expected memory format
 
-        Expected weight shape is (Cout, Cin, H, W).
-        The output shape is: (Cout, Cin_major, Bits, H x W, Cin_minor_bytes),
-        where Cin_major is the ceil(Cin / CIN_SUBTILE) and Cin_minor has to be padded with 0 to CIN_SUBTILE.
+        Expected weight shape is (cout, cin, height, width).
+        The output shape is: (cout, cinMajor, Bits, height x width, cinMinorBytes),
+        where cinMajor is the ceil(cin / CIN_SUBTILE) and cinMinor has to be padded with 0 to CIN_SUBTILE.
         """
         if depthwise:
-            weight = weight.transpose(1, 0, 2, 3)  # Swap Cout and Cin
+            weight = weight.transpose(1, 0, 2, 3)  # Swap cout and cin
 
-        Cout, Cin, H, W = weight.shape
+        cout, cin, height, width = weight.shape
 
-        # Pad Cin to be divisible with CIN_SUBTILE
-        if Cin % Ne16._CIN_SUBTILE != 0:
-            Cin_pad = Ne16._CIN_SUBTILE - Cin % Ne16._CIN_SUBTILE
+        # Pad cin to be divisible with CIN_SUBTILE
+        if cin % Ne16._CIN_SUBTILE != 0:
+            cin_pad = Ne16._CIN_SUBTILE - cin % Ne16._CIN_SUBTILE
             weight = np.pad(
                 weight,
-                ((0, 0), (0, Cin_pad), (0, 0), (0, 0)),
+                ((0, 0), (0, cin_pad), (0, 0), (0, 0)),
                 "constant",
                 constant_values=0,
             )
 
-        # Reshape into (Cout, Cin_major, Cin_minor, Flattened spatial, 1)
+        # Reshape into (cout, cinMajor, cinMinor, flattened spatial, 1)
         # The 1 at the end is required by the unpacking
-        Cin_major = int(np.ceil(Cin / Ne16._CIN_SUBTILE))
-        Cin_minor = Ne16._CIN_SUBTILE
-        weight = weight.reshape(Cout, Cin_major, Cin_minor, H * W, 1)
+        cinMajor = int(np.ceil(cin / Ne16._CIN_SUBTILE))
+        cinMinor = Ne16._CIN_SUBTILE
+        weight = weight.reshape(cout, cinMajor, cinMinor, height * width, 1)
 
         # Unpack 'bits' bits in little order, e.g. bits=4: 3 => [1, 1, 0, 0]
-        # (Cout, Cin_major, Cin_minor, Flattened spatial, Bits)
+        # (cout, cinMajor, cinMinor, flattened spatial, Bits)
         weight = np.unpackbits(weight, axis=-1, count=bits, bitorder="little")
 
         # Shuffle bits so that the final shape is:
-        # (Cout, Cin_major, Bits, Flattened spatial, Cin_minor)
+        # (cout, cinMajor, Bits, flattened spatial, cinMinor)
         weight = weight.transpose(0, 1, 4, 3, 2)
 
         # Prepare for packing
-        # (Cout, Cin_major, Bits, Flattened spatial, Cin_minor_bytes, 8)
-        Cin_minor_bytes = int(np.ceil(Cin_minor / 8))
-        weight = np.stack(np.split(weight, Cin_minor_bytes, axis=-1), axis=-2)
+        # (cout, cinMajor, Bits, flattened spatial, cinMinorBytes, 8)
+        cinMinorBytes = int(np.ceil(cinMinor / 8))
+        weight = np.stack(np.split(weight, cinMinorBytes, axis=-1), axis=-2)
 
         # Pack
-        # (Cout, Cin_major, Bits, Flattened spatial, Cin_minor_bytes)
+        # (cout, cinMajor, Bits, flattened spatial, cinMinorBytes)
         weight = np.packbits(weight, axis=-1, bitorder="little")
 
         return weight.flatten()
 
     @staticmethod
-    def weight_roll(weight: np.ndarray, bits: int, Cout: int, Cin: int, H: int, W: int):
+    def weight_roll(
+        weight: npt.NDArray[np.uint8],
+        bits: int,
+        cout: int,
+        cin: int,
+        height: int,
+        width: int,
+    ) -> npt.NDArray[np.uint8]:
         """Reverse of weight_roll"""
-        Cin_major = int(np.ceil(Cin / Ne16._CIN_SUBTILE))
-        Cin_minor = Ne16._CIN_SUBTILE
-        Cin_minor_bytes = int(np.ceil(Cin_minor / 8))
+        cinMajor = int(np.ceil(cin / Ne16._CIN_SUBTILE))
+        cinMinor = Ne16._CIN_SUBTILE
+        cinMinorBytes = int(np.ceil(cinMinor / 8))
 
-        weight = weight.reshape(Cout, Cin_major, bits, H * W, Cin_minor_bytes, 1)
+        weight = weight.reshape(cout, cinMajor, bits, height * width, cinMinorBytes, 1)
         weight = np.unpackbits(weight, axis=-1, count=8, bitorder="little")
-        weight = weight.reshape(Cout, Cin_major, bits, H * W, Cin_minor)
+        weight = weight.reshape(cout, cinMajor, bits, height * width, cinMinor)
         weight = weight.transpose(0, 1, 4, 3, 2)
         weight = np.packbits(weight, axis=-1, bitorder="little")
-        weight = weight.reshape(Cout, Cin_major * Cin_minor, H, W)
-        weight = weight[:, :Cin, :, :]
+        weight = weight.reshape(cout, cinMajor * cinMinor, height, width)
+        weight = weight[:, :cin, :, :]
 
         return weight
