8938d6a7a3899c57f7940a0bc0249701cca58f8a
Luka Macan <luka.macan@unibo.it>
2025-02-18 23:02:46 +0100

    Remove docker and use the github runner directly for neureka(-v2) tests

--- test/docker/get_pulp_riscv_gnu_toolchain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-curl -L https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2 > pulp-riscv-gnu-toolchain.tar.bz2
-mkdir toolchains
-tar -xvjf ./pulp-riscv-gnu-toolchain.tar.bz2 -C ./toolchains
-mv ./toolchains/v1.0.16-pulp-riscv-gcc-ubuntu-18 ./toolchains/pulp-riscv-gnu-toolchain
-rm ./pulp-riscv-gnu-toolchain.tar.bz2
