fab5942c8dfcc80515ce8a3f49838dc3cc71b806
Luka Macan <luka.macan@unibo.it>
2025-02-18 23:05:11 +0100

    Remove docker and use ubuntu github runners directly (#21)

--- test/docker/build_riscv_gnu_toolchain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# Build and install
-mkdir -p $TOOLCHAIN_GNU_INSTALL_DIR
-cd riscv-gnu-toolchain
-./configure --prefix=$TOOLCHAIN_GNU_INSTALL_DIR --with-arch=rv32imfcxpulpv3 --with-abi=ilp32 --enable-multilib
-make -j 8
